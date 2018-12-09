6a47fbe942f347f8542309fee58766009df1fc45
Malte Hübner <maltehuebner@gmx.org>
2018-12-09 17:05:29 +0100

    Init plugin.

---
+++ liveontrain.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+/*
+Plugin Name: Live on train
+Plugin URI: https://liveontrain.maltehuebner.com
+Description: Description to follow.
+Author: Malte Hübner
+Version: 0.1
+Author URI: https://maltehuebner.de/
+*/
