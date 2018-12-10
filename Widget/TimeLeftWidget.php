a348dd68851ec6e9f71ecd81dc6f72eb3dbc0a71
Malte Hübner <maltehuebner@gmx.org>
2018-12-10 22:24:31 +0100

    Simplify.

--- Widget/TimeLeftWidget.php
+++ Widget/TimeLeftWidget.php
@@ -6,6 +6,11 @@ use WP_Widget;
 
 class TimeLeftWidget extends WP_Widget
 {
+    protected $fieldList = [
+        'title' => 'Titel',
+        'dateTime' => 'Ziel-Zeitpunkt',
+    ];
+
     public function __construct()
     {
         parent::__construct(
@@ -37,32 +42,28 @@ class TimeLeftWidget extends WP_Widget
 
     public function form($instance): void
     {
-        $title = !empty($instance['title']) ? $instance['title'] : esc_html__('New title', 'caldera_journey');
-        $dateTime = !empty($instance['dateTime']) ? $instance['dateTime'] : esc_html__('New date time', 'caldera_journey');
+        foreach ($this->fieldList as $key => $field)
+        {
+            $value = !empty($instance[$key]) ? $instance[$key] : esc_html__(sprintf('%s neu', $field), 'caldera_journey');
 
-        ?>
-        <p>
-            <label for="<?php echo esc_attr($this->get_field_id('title')); ?>">
-                <?php esc_attr_e('Title:', 'caldera_journey'); ?>
-            </label>
-            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>" name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-        </p>
-
-        <p>
-            <label for="<?php echo esc_attr($this->get_field_id( 'dateTime')); ?>">
-                <?php esc_attr_e('Zeitpunkt:', 'caldera_journey'); ?>
-            </label>
-            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('dateTime')); ?>" name="<?php echo esc_attr($this->get_field_name('dateTime')); ?>" type="text" value="<?php echo esc_attr($dateTime); ?>" />
-        </p>
-        <?php
+            ?>
+            <p>
+                <label for="<?php echo esc_attr($this->get_field_id($key)); ?>">
+                    <?php esc_attr_e(sprintf('%s:', $field), 'caldera_journey'); ?>
+                </label>
+                <input class="widefat" id="<?php echo esc_attr($this->get_field_id($key)); ?>" name="<?php echo esc_attr($this->get_field_name($key)); ?>" type="text" value="<?php echo esc_attr($value); ?>" />
+            </p>
+            <?php
+        }
     }
 
     public function update($newInstance, $old_instance): array
     {
         $instance = array();
 
-        $instance['title'] = (!empty( $newInstance['title'])) ? sanitize_text_field($newInstance['title']) : '';
-        $instance['dateTime'] = (!empty( $newInstance['dateTime'])) ? sanitize_text_field($newInstance['dateTime']) : '';
+        foreach ($this->fieldList as $key => $field) {
+            $instance[$key] = (!empty( $newInstance[$key])) ? sanitize_text_field($newInstance[$key]) : '';
+        }
 
         return $instance;
     }
